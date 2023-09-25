5e14f9c75de2b827d6d0578fb3d0e97984d0ca8d
rabgonzalez <rubalba.rag@gmail.comgit config --global user.name rabgonzalez>
2023-09-25 16:27:53 +0100

    Añadimos ejercicios hasta el 5

---
+++ Unidad-1/tareas/tarea2/src/main/java/Ejercicio2.java
@@ -0,0 +1,13 @@
+/**
+ * @author rabgonzalez
+ * Ejercicio Numero 2
+ */
+import java.util.Scanner;
+public class Ejercicio2 {
+    /**
+     * Descripcion del algoritmo
+     * @param args
+     */
+    public static void main(String[] args) {
+    }
+}
