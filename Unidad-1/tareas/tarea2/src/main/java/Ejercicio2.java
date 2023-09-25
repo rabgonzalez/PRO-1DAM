4e3f97963d1e75899c1e5ced158f118742da6fb1
rabgonzalez <rubalba.rag@gmail.comgit config --global user.name rabgonzalez>
2023-09-25 18:38:33 +0100

    Solucion ejercicio 2

--- Unidad-1/tareas/tarea2/src/main/java/Ejercicio2.java
+++ Unidad-1/tareas/tarea2/src/main/java/Ejercicio2.java
@@ -10,8 +10,52 @@ public class Ejercicio2 {
      * Descripcion del algoritmo
      * @param args
      */
-    Scanner lectura = new Scanner(System.in);
-    public static void main(String[] args) {
-        int valorA= System.out.println("Introduzca el valor de A: ");
+
+// declarar variables
+    static Scanner lectura = new Scanner(System.in);
+    public static void main(String[] args){
+       int valorA;
+       int valorB;
+       int valorC;
+
+// mientras el if sea verdadero, se crea un bucle que pide valores distintos debido a que una de las variables se repite
+       while(true)
+       {
+           System.out.println("Ingresa el valor de A:");
+           valorA = lectura.nextInt();
+           System.out.println("Ingresa el valor de B:");
+           valorB = lectura.nextInt();
+           System.out.println("Ingresa el valor de C:");
+           valorC = lectura.nextInt();
+
+           if (valorA == valorB || valorA == valorC)
+           {
+               System.out.println("No repitas valores");
+           }
+        
+// si la condicion es falsa, rompemos el bucle y buscamos la variable mayor
+           else
+           {
+               break;
+           }
+       }
+
+// vemos si el valor de la variable A es el mayor
+       if (valorA > valorB && valorA > valorC)
+       {
+            System.out.println("A es el mayor");
+       }
+
+// vemos si el valor de la variable B es el mayor
+       else if(valorB > valorA && valorB > valorC)
+       {
+            System.out.println("B es el mayor");
+       }
+
+// Si ni A ni B son las mayores, daremos por mayor a la variable C
+       else
+       {
+            System.out.println("C es el mayor");
+        }
     }
 }
