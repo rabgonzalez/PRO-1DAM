f1c72bec73763bc900fb05ad6e9e280c5de83abc
rabgonzalez <rubalba.rag@gmail.comgit config --global user.name rabgonzalez>
2023-09-27 17:03:21 +0100

    hacer  ejercicio 4

--- Unidad-1/tareas/tarea2/src/main/java/Ejercicio4.java
+++ Unidad-1/tareas/tarea2/src/main/java/Ejercicio4.java
@@ -3,11 +3,25 @@
  * Ejercicio Numero 4
  */
 import java.util.Scanner;
-public class Ejercicio4 {
+public class Ejercicio4
+{
+    static Scanner lectura = new Scanner(System.in);
     /**
      * Descripcion del algoritmo
      * @param args
      */
-    public static void main(String[] args) {
+    public static void main(String[] args)
+    {
+        int Numero;
+        System.out.println("Introduce un número:");
+        Numero = lectura.nextInt();
+
+        if(Numero % 2 == 0)
+        {
+            System.out.println("El número " + Numero + " es par");
+        }
+        else{
+            System.out.println("El número " + Numero + " es impar");
+        }
     }
 }
