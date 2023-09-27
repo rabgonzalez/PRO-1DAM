03e65aad21f7a2dc322ea921136d5dc270dc41f7
rabgonzalez <rubalba.rag@gmail.comgit config --global user.name rabgonzalez>
2023-09-27 19:12:00 +0100

    correccion ejercicio 5

--- Unidad-1/tareas/tarea2/src/main/java/Ejercicio5.java
+++ Unidad-1/tareas/tarea2/src/main/java/Ejercicio5.java
@@ -22,9 +22,13 @@ public class Ejercicio5 {
         {
             System.out.println(numero1 + " es mayor que " + numero2);
         }
-        else
+        else if(numero1 < numero2)
         {
             System.out.println(numero2 + " es mayor que " + numero1);
+        }
+        else
+        {
+            System.out.println("Ambos números son iguales");
         }
     }
 }
