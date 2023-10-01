a10ebd0b56bd4ffe734b797e10c0c221eaf4b143
rabgonzalez <145138682+rabgonzalez@users.noreply.github.com>
2023-10-01 17:25:17 +0100

    empecé el ejercicio 8

--- Unidad-1/tareas/tarea2/src/main/java/Ejercicio8.java
+++ Unidad-1/tareas/tarea2/src/main/java/Ejercicio8.java
@@ -13,6 +13,15 @@ public class Ejercicio8 {
      */
     public static void main(String[] args)
     {
+        int sumatorio;
 
+        for(int i = 0; i <= numero; i++)
+        {
+            int numero;
+            System.out.println("Ingresa el valor: ");
+            numero = lectura.nextInt();
+            numero += sumatorio;
+        }
+        System.out.println(sumatorio / numero);
     }
 }
