f8498e4b67389d859a2d6c18abc6ede8b831527d
rabgonzalez <rubalba.rag@gmail.comgit config --global user.name rabgonzalez>
2023-10-02 16:41:11 +0100

    tarea2

---
+++ Unidad-1/tareas/tarea1/src/main/java/eercicio2.java
@@ -0,0 +1,33 @@
+
+
+public class eercicio2 {
+
+
+    /**
+     * Descripcion del algoritmo
+     * Un número perfecto es aquel que es igual a la suma de sus divisores propios (excluyendo él mismo). Por ejemplo, 6 es un número perfecto porque sus divisores propios son 1, 2 y 3, y 1 + 2 + 3 = 6.
+
+El ejercicio consiste en escribir un programa que encuentre e imprima todos los números perfectos en un rango dado (por ejemplo, entre 1 y 1000).
+     * @param args
+     */
+     
+    public static void main(String[] args)
+    {
+        int suma = 0;
+        for(int i = 1; i <= 1000; i++)
+        {
+            for(int n = 1; n <= i/2; n++)
+            {
+                if(i % n == 0)
+                {
+                    suma += n;
+                }
+            }
+            if(suma == i)
+            {
+                System.out.println(i);
+            }
+            suma = 0;
+        }
+    }
+}
