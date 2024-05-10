8d9f8d54b2f1682000633efcfc849192f51a5eba
rabgonzalez <rubalba.rag@gmail.com>
2024-05-10 17:48:37 +0100

    Mapper

--- Unidad-10/tareas/mappers-java/src/main/java/es/ies/puerto/mapper/struct/IMapperCustomer.java
+++ Unidad-10/tareas/mappers-java/src/main/java/es/ies/puerto/mapper/struct/IMapperCustomer.java
@@ -1,15 +1,27 @@
 package es.ies.puerto.mapper.struct;
 
+import org.mapstruct.Context;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.Named;
 import org.mapstruct.factory.Mappers;
 
 import es.ies.puerto.dto.CustomerDTO;
+import es.ies.puerto.model.entity.Address;
 import es.ies.puerto.model.entity.Customer;
 
 @Mapper(uses = IMapperAddress.class)
 public interface IMapperCustomer {
     IMapperCustomer INSTANCE = Mappers.getMapper(IMapperCustomer.class);
 
-    public CustomerDTO customerEntityToCustomerDto(Customer customer);
+    @Mapping(source = "address", target = "address")
+    @Mapping(source = "customer.id", target = "id")
+    @Mapping(source = "customer", target = "fullName", qualifiedByName = "transformarNombre")
+    public CustomerDTO customerEntityToCustomerDto(Address address, @Context Customer customer);
     public Customer customerDtoToCustomerEntity(CustomerDTO customerDTO);
+
+    @Named("transformarNombre")
+    public static String crearNombre(Customer customer){
+        return customer.getFirstname()+" "+customer.getLastname();
+    }
 }
