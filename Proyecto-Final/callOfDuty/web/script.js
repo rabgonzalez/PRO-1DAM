d554e01cc3c893c85d1127250973414958f01c8e
rabgonzalez <rubalba.rag@gmail.com>
2024-06-14 09:47:49 +0100

    getAll finsihed (needs optimization

--- Proyecto-Final/callOfDuty/web/script.js
+++ Proyecto-Final/callOfDuty/web/script.js
@@ -19,7 +19,28 @@ function fetchUrlGun(url) {
 function fetchUrlAgent(url) {
     fetch(url)
         .then(response => response.json())
-        .then(jsonObj => type(jsonObj))
+        .then(jsonObj => typeAgent(jsonObj))
+        .catch(() => alert('Algo no ha salido bien'))
+}
+
+function fetchUrlMap(url) {
+    fetch(url)
+        .then(response => response.json())
+        .then(jsonObj => typeMap(jsonObj))
+        .catch(() => alert('Algo no ha salido bien'))
+}
+
+function fetchUrlGamemode(url) {
+    fetch(url)
+        .then(response => response.json())
+        .then(jsonObj => typeGamemode(jsonObj))
+        .catch(() => alert('Algo no ha salido bien'))
+}
+
+function fetchUrlGame(url) {
+    fetch(url)
+        .then(response => response.json())
+        .then(jsonObj => typeGame(jsonObj))
         .catch(() => alert('Algo no ha salido bien'))
 }
 
@@ -33,17 +54,56 @@ function fetchAgents() {
     fetchUrlAgent(url);
 }
 
+function fetchMaps() {
+    let url = 'http://localhost:8080/api-cod/map/';
+    fetchUrlMap(url);
+}
+
+function fetchGamemodes() {
+    let url = 'http://localhost:8080/api-cod/gamemode/';
+    fetchUrlGamemode(url);
+}
+
+function fetchGames() {
+    let url = 'http://localhost:8080/api-cod/game/';
+    fetchUrlGame(url);
+}
+
 function typeGun(object) {
-    if (object.length = 1) {
+    if (object.length >= 1) {
         getGuns(object)
     } else {
         console.log('Esto esta vacío')
     }
 }
 
-function type(object) {
-    if (object.length > 1) {
+function typeAgent(object) {
+    if (object.length >= 1) {
         getAgents(object)
+    } else {
+        console.log('Esto esta vacío')
+    }
+}
+
+function typeMap(object) {
+    if (object.length >= 1) {
+        getMaps(object)
+    } else {
+        console.log('Esto esta vacío')
+    }
+}
+
+function typeGamemode(object) {
+    if (object.length >= 1) {
+        getGamemodes(object)
+    } else {
+        console.log('Esto esta vacío')
+    }
+}
+
+function typeGame(object) {
+    if (object.length >= 1) {
+        getGames(object)
     } else {
         console.log('Esto esta vacío')
     }
@@ -101,8 +161,79 @@ function getGuns(guns){
         <td>RARITY</td>
         <th>${gun.rarity}</th>
         </tr>
+        </table>`
+    });
+    const agentsZone = document.getElementById('getAll');
+    agentsZone.innerHTML = objects;
+}
+
+function getMaps(maps){
+    let objects = ``
+    maps.forEach(map => {
+        objects += `
+        <table>
         <tr>
-        
+        <td>ID</td>
+        <th>${map.id}</th>
+        </tr>
+        <tr>
+        <td>NAME</td>
+        <th>${map.name}</th>
+        </tr>
+        <tr>
+        <td>COUNTRY</td>
+        <th>${map.country}</th>
+        </tr>
+        <tr>
+        <td>GAME</td>
+        <th>${map.game_id}</th>
+        </tr>
+        </table>`
+    });
+    const agentsZone = document.getElementById('getAll');
+    agentsZone.innerHTML = objects;
+}
+
+function getGamemodes(gamemodes){
+    let objects = ``
+    gamemodes.forEach(gamemode => {
+        objects += `
+        <table>
+        <tr>
+        <td>ID</td>
+        <th>${gamemode.id}</th>
+        </tr>
+        <tr>
+        <td>NAME</td>
+        <th>${gamemode.name}</th>
+        </tr>
+        </table>`
+    });
+    const agentsZone = document.getElementById('getAll');
+    agentsZone.innerHTML = objects;
+}
+
+function getGames(games){
+    let objects = ``
+    games.forEach(game => {
+        objects += `
+        <table>
+        <tr>
+        <td>ID</td>
+        <th>${game.id}</th>
+        </tr>
+        <tr>
+        <td>NAME</td>
+        <th>${game.gameName}</th>
+        </tr>
+        <tr>
+        <td>RELEASE</td>
+        <th>${game.releaseDate}</th>
+        </tr>
+        <tr>
+        <td>RATING</td>
+        <th>${game.rating}</th>
+        </tr>
         </table>`
     });
     const agentsZone = document.getElementById('getAll');
